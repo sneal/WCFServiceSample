af1589c2d691044a39b860e61200c551fe5096d7
Shawn Neal <sneal@sneal.net>
2017-12-01 09:18:21 -0800

    Add extensionless routing using aspnet routing

---
+++ Sneal.Service.IIS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sneal.Service.IIS.Global" Language="C#" %>
---
+++ Sneal.Service.IIS/Global.asax.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Web.Routing;
+using System.ServiceModel.Activation;
+
+namespace Sneal.Service.IIS
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RouteTable.Routes.Add(new ServiceRoute("Hello", new ServiceHostFactory(), typeof(HelloService)));
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
